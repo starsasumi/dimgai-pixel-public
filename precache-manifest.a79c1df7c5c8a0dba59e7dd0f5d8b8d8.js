3a08279d9b5e8810b1a18477a23a12b656c637eb
SHI Xiaxing <starsasumi@gmail.com>
2020-11-12 10:53:48 +0100

    Init

---
+++ precache-manifest.a79c1df7c5c8a0dba59e7dd0f5d8b8d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6099e7b885f992169cfb82a652bd2133",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3d9dee8fc0a58eed44d",
+    "url": "./static/css/main.33971239.chunk.css"
+  },
+  {
+    "revision": "4813defb5ac6c5c848fa",
+    "url": "./static/js/2.36eea004.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "./static/js/2.36eea004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d9dee8fc0a58eed44d",
+    "url": "./static/js/main.8ae2f89e.chunk.js"
+  },
+  {
+    "revision": "fc6f2a30e7fa5d7e717b",
+    "url": "./static/js/runtime-main.96ebbc1e.js"
+  },
+  {
+    "revision": "08c166a8e963d1285e6c43f2194cbf7d",
+    "url": "./static/media/transparent.08c166a8.svg"
+  }
+]);
